c8f116b8bfb655a9eee7da7bb0edac1a738c8fa0
Bat-Erdene912 <erdenee0912@gmail.com>
2025-04-08 18:13:26 +0800

    Update Flashcard.java

--- src/main/java/com/example/Flashcard.java
+++ src/main/java/com/example/Flashcard.java
@@ -1,1 +1,22 @@
+public class Flashcard {
+    private String question;
+    private String answer;
+    private int mistakeCount;
+    private int correctCount;
 
+    public Flashcard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+        this.mistakeCount = 0;
+        this.correctCount = 0;
+    }
+
+    public String getQuestion() { return question; }
+    public String getAnswer() { return answer; }
+    
+    public void markCorrect() { correctCount++; }
+    public void markIncorrect() { mistakeCount++; }
+    
+    public int getMistakeCount() { return mistakeCount; }
+    public int getCorrectCount() { return correctCount; }
+}
